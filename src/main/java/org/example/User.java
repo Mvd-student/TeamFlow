e45db33cf47ef8df2eb4c70f13df3bcebaf771db
Teun Klaassens <w.t.klaassens@student.tudelft.nl>
2025-04-07 10:59:37 +0200

    Add User class and project setup files

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,57 @@
+package org.example;
+
+public class User {
+    private int id;
+    private String username;
+    private String name;
+    private String password;
+
+    public User(int id, String username, String name, String password) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
