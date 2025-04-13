ed3dcfdca7b8bf54a1626dad2d390a3075ebac9c
Teun Klaassens <w.t.klaassens@student.tudelft.nl>
2025-04-13 21:23:02 +0200

    voeg printMessages-methode toe voor het tonen van chatberichten in de CLI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,33 @@
 package org.example;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
     }
+
+
+
+
+
+    public static void printMessages(List<Message> messages, int currentUserId, java.util.Map<Integer, String> userIdToUsername) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        for (Message msg : messages) {
+            String tijd = msg.getDateTime().format(formatter);
+            String afzender;
+
+            if (msg.getUserId() == currentUserId) {
+                afzender = "Jij";
+            } else {
+                afzender = userIdToUsername.getOrDefault(msg.getUserId(), "Onbekend");
+            }
+
+            System.out.println("[" + tijd + "] " + afzender + ": " + msg.getContent());
+        }
+    }
+
 }
