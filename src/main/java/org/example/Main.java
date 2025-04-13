7242187c9897ce01f3adc8fa8fe4848d69024ec5
Teun Klaassens <w.t.klaassens@student.tudelft.nl>
2025-04-13 21:29:00 +0200

    Remove unnecessary code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,8 +14,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
